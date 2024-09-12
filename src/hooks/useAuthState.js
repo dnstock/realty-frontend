1699a8f344eb4b93abc2daea405cf8ba4a3e3eba
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-09-11 20:08:42 -0400

    Add callbacks and dependency injection for stability, move useApi call to after logout function definition

--- src/hooks/useAuthState.js
+++ src/hooks/useAuthState.js
@@ -1,11 +1,10 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useCallback } from 'react';
 import authService from '../services/AuthService';
 import { useApi } from './useApi';
 import { useNavigate } from 'react-router-dom';
 
 export const useAuthState = () => {
   const navigate = useNavigate();
-  const api = useApi(logout); // Pass logout callback
 
   const [authState, setAuthState] = useState(() => ({
     accessToken: authService.getAccessToken(),
@@ -13,29 +12,14 @@ export const useAuthState = () => {
     user: authService.getUser(),
   }));
 
-  const refreshToken = async () => {
-    if (!authState.refreshToken) return;
-    try {
-      const { access_token } = await api.tokenRefresh();
-      setAuthState((prevState) => ({
-        ...prevState,
-        accessToken: access_token,
-      }));
-      AuthService.setAuthData(access_token, authState.refreshToken, authState.user);
-    } catch (error) {
-      console.error('Failed to refresh token', error);
-      logout();
-    }
-  };
-
-  const login = (authToken, refreshToken, userData) => {
+  const login = useCallback((authToken, refreshToken, userData) => {
     setAuthState({ accessToken: authToken, refreshToken: refreshToken, user: userData });
-  };
     authService.setAuthData(authToken, refreshToken, userData);
+  }, []);
 
   // redirectDelay is used to add a delay (in seconds) before redirecting to the login page
   // 0 = no delay (default), -1 = do not redirect (useful for testing), >0 = delay in seconds
-  const logout = (redirectDelay = 0) => {
+  const logout = useCallback((redirectDelay = 0, redirectUrl = '/login') => {
     setAuthState({ accessToken: null, refreshToken: null, user: null });
     authService.clearAuthData();
 
@@ -43,20 +27,37 @@ export const useAuthState = () => {
     if (redirectDelay === -1) return;
 
     // Redirect after the specified delay and return cleanup function
-    const timer = setTimeout(() => navigate('/login'), redirectDelay * 1000);
+    const timer = setTimeout(() => navigate(redirectUrl), redirectDelay * 1000);
     return () => clearTimeout(timer);
-  };
+  }, [navigate]);
 
-  const isAuthenticated = () => {
-    return !!authState.accessToken && !AuthService.isTokenExpired(authState.accessToken);
-  };
+  const api = useApi(logout); // Pass logout callback
+
+  const refreshToken = useCallback(async () => {
+    if (!authState.refreshToken) return;
+    try {
+      const { access_token } = await api.tokenRefresh();
+      setAuthState((prevState) => ({
+        ...prevState,
+        accessToken: access_token,
+      }));
+      authService.setAuthData(access_token, authState.refreshToken, authState.user);
+    } catch (error) {
+      console.error('Failed to refresh token', error);
+      logout();
+    }
+  }, [authState.refreshToken, authState.user, api, logout]);
+
+  const isAuthenticated = useCallback(() => {
+    return !!authState.accessToken && !authService.isTokenExpired(authState.accessToken);
+  }, [authState.accessToken]);
 
   // Check and refresh token on load or when accessToken changes
   useEffect(() => {
     if (authState.accessToken && authService.isTokenExpired(authState.accessToken)) {
       refreshToken();
     }
-  }, [authState.accessToken]);
+  }, [authState.accessToken, refreshToken]);
 
   const authContextValue = useMemo(
     () => ({
@@ -67,7 +68,7 @@ export const useAuthState = () => {
       refreshToken,
       isAuthenticated,
     }),
-    [authState]
+    [authState, api, login, logout, refreshToken, isAuthenticated]
   );
 
   return authContextValue;
