11bdf95aba3fac891058f05933a78ba25e4f96f4
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-11-08 20:43:38 -0500

    Small optimization to dynamic export

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,9 @@ const ctx = require.context('./', false, /^\..+\.js$/);
 const pages = ctx.keys().reduce((modules, path) => {
    // Remove './' and '.js' from filenames
   const moduleName = path.replace(/^\.\/|\.js$/g, '');
-  // Skip this file
-  if (moduleName === 'index') return modules;
-  return {
-    ...modules,
-    [moduleName]: ctx(path).default,
-  };
+  if (moduleName === 'index') return modules; // Skip index.js (this file)
+  Object.assign(modules, { [moduleName]: ctx(path).default });
+  return modules;
 }, {});
 
 export default pages;
