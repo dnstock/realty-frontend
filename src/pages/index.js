201c051004ceeb17e91c7e9f9a38ac29e6ea9069
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2024-11-13 12:12:23 -0500

    Restrict dynamic import of Page components to filenames starting with a capital letter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
+// Dynamically import all Page files from the current folder
+// Page filenames *must* begin with a capital letter
+
 // Avoid duplication (e.g. './Home.js' and 'pages/Home.js')
-const ctx = require.context('./', false, /^\..+\.js$/);
+const ctx = require.context('./', false, /^\.\/[A-Z].+\.js$/);
 
 const pages = ctx.keys().reduce((modules, path) => {
    // Remove './' and '.js' from filenames
   const moduleName = path.replace(/^\.\/|\.js$/g, '');
-  if (moduleName === 'index') return modules; // Skip index.js (this file)
   Object.assign(modules, { [moduleName]: ctx(path).default });
   return modules;
 }, {});
