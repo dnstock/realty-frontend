249c6f0d32a0d8fd8c5272b269697f2b86aaf07b
Dan Harcsztark <85728+dnstock@users.noreply.github.com>
2025-01-16 23:51:49 -0500

    Update Building resource: Add vacancy and occupancy data

--- src/config/resources/building.js
+++ src/config/resources/building.js
@@ -19,7 +19,11 @@ export const building = {
   columns: [
     { field: 'id', flex: 0.5, minWidth: 50 },
     { field: 'name', flex: 2, minWidth: 100 },
-    { field: 'unit_count', headerName: 'Total Units', flex: 1, minWidth: 90 },
+    { field: 'unit_count', headerName: 'Units', flex: 1, minWidth: 90 },
+    { field: 'vacancy', flex: 1.5, minWidth: 90,
+      valueGetter: (params) => `${params.count} units / ${(params.rate * 100).toFixed(2)}%` },
+    { field: 'occupancy', flex: 1.5, minWidth: 90,
+      valueGetter: (params) => `${params.count} units / ${(params.rate * 100).toFixed(2)}%` },
     { field: 'property', flex: 2, minWidth: 100,
       valueGetter: (params) => params.name || '[ Error ]', },
   ],
